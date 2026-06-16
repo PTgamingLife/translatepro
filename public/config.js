63876546a25485fc3352ad5326163c01224bd0ad
poting75321-commits <poting75321@gmail.com>
2026-06-16 09:25:02 +0800

    Configure Supabase project for Pages

--- public/config.js
+++ public/config.js
@@ -10,4 +10,6 @@
 // window.TRANSLATEPRO_CONFIG = {
 //   edgeFunctionUrl: "https://abcdefghijklmnopqrst.supabase.co/functions/v1/batch-translate"
 // };
-window.TRANSLATEPRO_CONFIG = window.TRANSLATEPRO_CONFIG || {};
+window.TRANSLATEPRO_CONFIG = {
+  supabaseProjectRef: "hhcubvixldieuwdeqnwc"
+};
